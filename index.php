697dcc4b3e4c91a622e044589c516c448bbc0db9
Hannes A. J <hannes+github@agnarsson.com>
2015-07-07 12:32:50 +0000

    Current version as of 2015-07-07

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>The Big List of Icelandic Developers</title>
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="This is a list of developers in Iceland - in case you need an awesome person to do some developing or just want to see what's going on in the Icelandic development community."/>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+	<link rel="stylesheet" type="text/css" href="/projects/biglist/developers/style.css"/>
+	<meta property="og:image" content="http://hannes.agnarsson.com/projects/biglist/developers/icedev-og-image.gif"/>
+	<meta property="og:title" content="The Big List of Icelandic Developers"/>
+	<meta property="og:description" content="This is a list of developers in Iceland - in case you need an awesome person to do some developing or just want to see what's going on in the Icelandic development community."/>
+	<meta property="og:url" content="http://hannes.agnarsson.com/projects/biglist/developers/"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:site_name" content="hannes.agnarsson.com"/>
+	<meta property="fb:admins" content="336300002"/>
+
+	<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-11714677-1']);
+		  _gaq.push(['_setDomainName', '.agnarsson.com']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+	</script>
+
+	<script src="http://platform.twitter.com/anywhere.js?id=6urooIiWnr27WCTfn7mA&amp;v=1">
+	</script>
+	<script type="text/javascript">
+	   twttr.anywhere(function(twitter) {
+	            twitter.hovercards();
+
+	   });
+	</script>
+
+</head>
+<body>
+	
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div id="container">
+
+		<h1>The Big List of Icelandic Developers</h1>
+
+		<p>Do you need an awesome person to do some developing? Or do you just want to see what's going on in the Icelandic development community? This is a growing list of developers in Iceland... so hopefully it'll be big soon ;)</p>
+
+		<p>Do you think you should be on the list or do you know a developer that isn't on the list? Send in the info (name, personal website &amp; Twitter username) using <a href="http://www.officialstation.com/contact" title="Submit the info through this form please">this form</a> or send an e-mail to hannes at the domain agnarsson dot com (if you know what I mean).</p>
+		
+		<div class="fb-like" data-href="http://hannes.agnarsson.com/projects/biglist/developers/" data-send="true" data-width="550" data-show-faces="true" data-font="arial" ref="biglistdev-top"></div>
+		
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://hannes.agnarsson.com/projects/biglist/developers/" data-via="officialstation" data-size="large" data-related="doncomodo:Awesome t-shirts that look amazing and are incredibly comfortable,reciary:Your recipe diary to share what you make and be inspired by others" data-hashtags="icedev">Tweet</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+		<h2>Developers</h2>
+		
+		<!-- New people on the list are marked with * New * for ~1 month -->
+
+		<a href="http://iceland.aitorgarciarey.com/">Aitor García Rey</a> | @_aitor<br />
+		<a href="http://andrioid.net/">Andri Óskarsson</a> | @andrioid<br />
+		<a href="http://vefstofan.is">Andri Sigurðsson</a> | @andrisig<br />
+		<a href="http://www.linkedin.com/in/arighauks">Ari Geir Hauksson</a> <span class="new" title="Added: 2012-05-01">* New *</span><br />
+		<a href="http://www.flassari.is/">Ari Þór H. Arnbjörnsson</a> | @flassari<br />
+		<a href="http://arnorhs.com/">Arnór Heiðar</a> | @arnorhs<br />
+		<a href="http://www.axelrafn.org/">Axel Rafn Benediktsson</a> | @axelrafn<br />
+		<a href="http://monadic.io/">Árni Hermann Reynisson</a> | @arnihermann <span class="new" title="Added: 2012-05-01">* New *</span><br />
+		<a href="http://www.pronasty.com/">Árni Jónsson</a> | @addininja<br />
+		Baldur Már Helgason | @baldurhelgason <span class="new" title="Added: 2012-05-01">* New *</span><br />
+		<a href="mailto:aldur29[at]gmail.com">Baldur Þorgilsson</a> <span class="new" title="Added: 2012-05-01">* New *</span><br />		
+		<a href="http://www.bmson.com/">Baldvin Mar Smárason</a> | @bmson<br />
+		Berglind Bergsdóttir | @beggaboo85<br />
+		Bergur Hallgrímsson | @bergur <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		<a href="http://bre.klaki.net/">Bjarni Rúnar Einarsson</a> | @HerraBRE<br />
+		<a href="http://www.bingimar.is/">Bjarni Ingimar Júlíusson</a> | @bingimar<br />
+		<a href="http://bjarni.net/">Bjarni Sævarsson</a> | @bjarnis<br />
+		<a href="http://www.csc.kth.se/~bjoola/">Björgvin Ólafsson</a> | @bjorgvino<br />
+		Björgvin Ragnarsson | @nifgraup<br />
+		<a href="http://www.undo.com/bjorn">Björn Friðgeir Björnsson</a> | @bjornfr<br />
+		Björn Ingimundarson | @bjossi <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		<a href="http://bthj.is/">Björn Þór Jónsson</a> | @bthj<br />
+		<a href="http://suda.co.uk">Brian Suda</a> | @briansuda<br />
+		<a href="http://egill.re/">Egill R. Erlendsson</a> | @e18n <span class="new" title="Added: 2012-05-01">* New *</span><br />		
+		<a href="http://neu-r.com">Einar Birgir Einarsson</a> | @einarbirgir<br />
+		Einar Kristján Bridde | @einarkristjan <span class="new" title="Added: 2012-05-02">* New *</span><br />		
+		<a href="http://beautyvsfunction.com/">Einar Þór Gústafsson</a> | @einargustafsson<br />
+		<a href="http://einarolafsson.com/">Einar Ólafsson</a> | @einsiol<br />
+		Erling Ormar Vignisson | @erlingormar<br />
+		<a href="http://fannar.me/">Fannar Freyr Jónsson</a> | @fannar<br />
+		<a href="http://www.gommit.com">Finnur Pálmi Magnússon</a> | @gommit<br />
+		<a href="http://g.is/">Garðar Hauksson</a> | @gardarh<br />		
+		<a href="http://geirfreysson.com/">Geir Freysson</a> | @geirfreysson<br />
+		<a href="http://gudmundurbjarni.com">Guðmundur Bjarni Ólafsson</a> | @gudmundur<br />
+		<a href="http://hreidarsson.com/">Guðmundur Hreiðarsson</a> | @gudmundurh<br />
+		Guðjón Már Guðjónsson | @gudjon<br />
+		<a href="http://gummih.com/">Gummi H</a> | @gummih<br />
+		<a href="http://www.hallakol.com/">Halla Kolbeinsdóttir</a> | @hallakol<br />
+		<a href="http://www.dojopan.net/">Halldór Örn Guðnason</a> | @dojopan<br />
+		<a href="http://hannes.agnarsson.com/">Hannes Agnarsson Johnson</a> | @officialstation<br />
+		<a href="http://www.helgi.me">Helgi Hrafn Halldórsson</a> | @harabanar<br />
+		<a href="http://hjalli.com/">Hjálmar Gíslason</a> | @hjalli<br />
+		<a href="http://www.hjaltijakobsson.com">Hjalti Jakobsson</a> | @hjaltij<br />
+		<a href="http://hlodver.com/">Hlöðver Þór Árnason</a> | @hlodver<br />
+		Hreinn Gustavsson | @hreinngu<br />
+		<a href="http://hugi.karlmenn.is/">Hugi Þórðarson</a> | @hugith<br />
+		<a href="http://ingig.net/">Ingi Gauti Ragnarsson</a> | @ingig<br />
+		Jakob Árni H. Ísleifsson | @jakobarni <span class="new" title="Added: 2012-05-10">* New *</span><br /> 
+		<a href="http://jongretar.com/">Jón Grétar Borgþórsson</a> | @JonGretar<br />
+		<a href="http://jonrh.is/">Jón Rúnar Helgason</a> | @jonrh <span class="new" title="Added: 2012-05-02">* New *</span><br />		
+		Jökull Sólberg Auðunsson | @jokull <span class="new" title="Added: 2012-05-01">* New *</span><br />
+		<a href="http://karltryggvason.com/">Karl Tryggvason</a> @karltryggvason<br />
+		<a href="http://photo.blog.is/">Kjartan Pétur Sigurðsson</a> | @photo_is <span class="new" title="Added: 2012-05-01">* New *</span></a><br /> 
+		<a href="http://www.kjartansverrisson.com/">Kjartan Sverrisson</a> | @Kjartans<br />
+		<a href="mailto:kb[at]tct.is">Kristján B Ómarsson</a> <span class="new" title="Added: 2012-05-01">* New *</span><br />
+		Kristján Ingi Mikaelsson | @kristjanmik <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		<a href="http://kristjan.info/">Kristján Ævarsson</a> | @bimmi<br />
+		<a href="http://www.trymbill.is/">Maggi Trymbill</a> | @trymbill<br />
+		<a href="http://www.acoustickitty.me/">Magnús Þór Jónsson</a> | @ekkimegas<br />
+		Marco Bancale <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		Marinó A. Jónsson | @marinoj<br />
+		<a href="http://mar.anomy.net/">Már Örlygsson</a> | @maranomynet<br />
+		Ólafur Jens Ólafsson | @olijay <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		<a href="http://olioskar.is/">Ólafur Óskar Egilsson</a> | @olioskar <span class="new" title="Added: 2012-05-01">* New *</span><br />		
+		<a href="http://ragnarfjolnisson.is/">Ragnar Fjölnisson</a> | @rfjolnisson <span class="new" title="Added: 2012-05-10">* New *</span><br />		
+		<a href="http://reynir.net/">Reynir Þ. Hübner</a> | @reynirhubner<br />
+		Sigtryggur A. Árnason | @sigtryggur<br />
+		<a href="http://www.svanbergsson.com/">Sigurður Árni Svanbergsson</a> | @siggiarni<br />
+		<a href="mailto:sigurjonvidar[the-at-sign]gmail[dot-here]com">Sigurjón Viðar Svavarsson</a> | @Sjonni1<br />
+		<a href="http://sindri.info/">Sindri Traustason</a> | @sindrit<br />
+		<a href="http://www.smarimccarthy.com/">Smári McCarthy</a> | @smarimc<br />
+		<a href="http://www.facebook.com/stefan.baxter">Stefán Baxter</a><br />
+		Stefán Vignir Skarphéðinsson | @stefanvignir<br />
+		<a href="mailto:styrmir[at]tolvumidlun.is">Styrmir Bjarnason</a> <span class="new" title="Added: 2012-05-01">* New *</span><br />		
+		Sverrir Ásmundsson | @tsverrir <span class="new" title="Added: 2012-05-10">* New *</span><br />
+		<a href="http://www.tinna.com/">Tinna Karen Gunnarsdóttir</a> | @tinnakaren<br />
+		<a href="http://tomas.arni.jonasson.name/">Tómas Árni Jónasson</a><br />
+		<a href="http://www.tryggvilarusson.com/">Tryggvi Lárusson</a> | @tryggvil<br />
+		<a href="http://www.facebook.com/valursve">Valur Sverrisson</a> | @valur<br />
+		Vernharður R. Sigurðsson | @vernhardur<br />
+		Viðar Svansson | @pjesi<br />
+		Vignir Jónsson | @vignir<br />
+		<a href="http://www.hugarorar.com/">Þórður Matthiasson</a> | @doddi79<br />
+		<a href="http://thorey.runarsdott.ir/">Þórey Rúnarsdóttir</a> | @thoreyrunars <span class="new" title="Added: 2012-05-02">* New *</span><br />
+		Ægir Finnsson | @finnsson<br />
+		<a href="http://www.thorsteinsson.is/">Ægir Þorsteinsson</a> | @thorsteinsson<br />
+		Ævar Örn Kvaran | @Skrekkur<br />
+
+		<p id="twitter-list">You can also check out the list on Twitter: <a href="http://twitter.com/officialstation/icelandic-developers" title="You can follow them all on Twitter">@officialstation/icelandic-developers</a></p>
+
+		<p id="inspiration">This is of course totally inspired by Ragnar Freyr's <a href="http://www.ragnarfreyr.com/biglist/" title="My muse ;)">The Big List of Icelandic Designers and Creatives</a>.</p>
+
+		<p id="sharing">Sharing is caring...</p>
+
+		<!-- AddThis Button BEGIN -->
+		<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+		<a class="addthis_button_preferred_1"></a>
+		<a class="addthis_button_preferred_2"></a>
+		<a class="addthis_button_preferred_3"></a>
+		<a class="addthis_button_preferred_4"></a>
+		<a class="addthis_button_compact"></a>
+		</div>
+		<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
+		<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=officialstation"></script>
+		<!-- AddThis Button END -->
+
+		<hr />
+
+		<div id="footer">
+			<a href="http://hannes.agnarsson.com"><img src="http://www.gravatar.com/avatar/<?php echo md5( strtolower( trim( "hannes@agnarsson.com" ) ) ); ?>?s=30" alt="Hannes - avatar" class="footer-avatar" /></a>
+			<a href="http://hannes.agnarsson.com" title="Click here to find out more about me...">Hannes</a> made this. <br />Feel free to send me <a href="http://twitter.com/?status=@officialstation%20about%20the%20the%20big%20list%20of%20developers..." target="_blank" title="Feedback through Twitter">feedback</a>.
+		</div>
+
+	</div>
+
+	<!-- Start of StatCounter Code -->
+	<script type="text/javascript">
+	var sc_project=3911935;
+	var sc_invisible=1;
+	var sc_security="68b2378c";
+	</script>
+
+	<script type="text/javascript"
+	src="http://www.statcounter.com/counter/counter.js"></script><noscript><div
+	class="statcounter"><a title="counter customizable"
+	href="http://statcounter.com/free_hit_counter.html"
+	target="_blank"><img class="statcounter"
+	src="http://c.statcounter.com/3911935/0/68b2378c/1/"
+	alt="counter customizable" ></a></div></noscript>
+	<!-- End of StatCounter Code -->
+
+	<script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
+	<script type="text/javascript">
+	try {
+	reinvigorate.track("b6obm-0eh89n761c");
+	} catch(err) {}
+	</script>
+
+</body>
+</html>
