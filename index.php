f2d8539cbaf6047afd591559f15904887e7c594e
Hannes A. J <hannesjohnson@users.noreply.github.com>
2015-07-27 20:22:28 +0000

    Added Twitter mini-profiles
    To replace the @Anywhere function.

--- index.php
+++ index.php
@@ -62,94 +62,94 @@
 		
 		<!-- New people on the list are marked with * New * for ~1 month -->
 
-		<a href="http://iceland.aitorgarciarey.com/">Aitor García Rey</a> | @_aitor<br />
-		<a href="http://andrioid.net/">Andri Óskarsson</a> | @andrioid<br />
-		<a href="http://vefstofan.is">Andri Sigurðsson</a> | @andrisig<br />
+		<a href="http://iceland.aitorgarciarey.com/">Aitor García Rey</a> | <a href="https://twitter.com/intent/user?screen_name=@_aitor" title="@_aitor on Twitter">@_aitor</a><br />
+		<a href="http://andrioid.net/">Andri Óskarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@andrioid" title="@andrioid on Twitter">@andrioid</a><br />
+		<a href="http://vefstofan.is">Andri Sigurðsson</a> | <a href="https://twitter.com/intent/user?screen_name=@andrisig" title="@andrisig on Twitter">@andrisig</a><br />
 		<a href="http://www.linkedin.com/in/arighauks">Ari Geir Hauksson</a><br />
-		<a href="http://www.flassari.is/">Ari Þór H. Arnbjörnsson</a> | @flassari<br />
-		<a href="http://arnorhs.com/">Arnór Heiðar</a> | @arnorhs<br />
-		<a href="http://www.axelrafn.org/">Axel Rafn Benediktsson</a> | @axelrafn<br />
-		<a href="http://monadic.io/">Árni Hermann Reynisson</a> | @arnihermann<br />
-		<a href="http://www.pronasty.com/">Árni Jónsson</a> | @addininja<br />
-		Baldur Már Helgason | @baldurhelgason<br />
+		<a href="http://www.flassari.is/">Ari Þór H. Arnbjörnsson</a> | <a href="https://twitter.com/intent/user?screen_name=@flassari" title="@flassari on Twitter">@flassari</a><br />
+		<a href="http://arnorhs.com/">Arnór Heiðar</a> | <a href="https://twitter.com/intent/user?screen_name=@arnorhs" title="@arnorhs on Twitter">@arnorhs</a><br />
+		<a href="http://www.axelrafn.org/">Axel Rafn Benediktsson</a> | <a href="https://twitter.com/intent/user?screen_name=@axelrafn" title="@axelrafn on Twitter">@axelrafn</a><br />
+		<a href="http://monadic.io/">Árni Hermann Reynisson</a> | <a href="https://twitter.com/intent/user?screen_name=@arnihermann" title="@arnihermann on Twitter">@arnihermann</a><br />
+		<a href="http://www.pronasty.com/">Árni Jónsson</a> | <a href="https://twitter.com/intent/user?screen_name=@addininja" title="@addininja on Twitter">@addininja</a><br />
+		Baldur Már Helgason | <a href="https://twitter.com/intent/user?screen_name=@baldurhelgason" title="@baldurhelgason on Twitter">@baldurhelgason</a><br />
 		<a href="mailto:aldur29[at]gmail.com">Baldur Þorgilsson</a><br />		
-		<a href="http://www.bmson.com/">Baldvin Mar Smárason</a> | @bmson<br />
-		Berglind Bergsdóttir | @beggaboo85<br />
-		Bergur Hallgrímsson | @bergur<br />
-		<a href="http://bre.klaki.net/">Bjarni Rúnar Einarsson</a> | @HerraBRE<br />
-		<a href="http://www.bingimar.is/">Bjarni Ingimar Júlíusson</a> | @bingimar<br />
-		<a href="http://bjarni.net/">Bjarni Sævarsson</a> | @bjarnis<br />
-		<a href="http://www.csc.kth.se/~bjoola/">Björgvin Ólafsson</a> | @bjorgvino<br />
-		Björgvin Ragnarsson | @nifgraup<br />
-		<a href="http://www.undo.com/bjorn">Björn Friðgeir Björnsson</a> | @bjornfr<br />
-		Björn Ingimundarson | @bjossi<br />
-		<a href="http://bthj.is/">Björn Þór Jónsson</a> | @bthj<br />
-		<a href="http://suda.co.uk">Brian Suda</a> | @briansuda<br />
-		<a href="http://egill.re/">Egill R. Erlendsson</a> | @e18n<br />		
-		<a href="http://neu-r.com">Einar Birgir Einarsson</a> | @einarbirgir<br />
-		Einar Kristján Bridde | @einarkristjan<br />		
-		<a href="http://beautyvsfunction.com/">Einar Þór Gústafsson</a> | @einargustafsson<br />
-		<a href="http://einarolafsson.com/">Einar Ólafsson</a> | @einsiol<br />
-		Erling Ormar Vignisson | @erlingormar<br />
-		<a href="http://fannar.me/">Fannar Freyr Jónsson</a> | @fannar<br />
-		<a href="http://www.gommit.com">Finnur Pálmi Magnússon</a> | @gommit<br />
-		<a href="http://g.is/">Garðar Hauksson</a> | @gardarh<br />		
-		<a href="http://geirfreysson.com/">Geir Freysson</a> | @geirfreysson<br />
-		<a href="http://gudmundurbjarni.com">Guðmundur Bjarni Ólafsson</a> | @gudmundur<br />
-		<a href="http://hreidarsson.com/">Guðmundur Hreiðarsson</a> | @gudmundurh<br />
-		Guðjón Már Guðjónsson | @gudjon<br />
-		<a href="http://gummih.com/">Gummi H</a> | @gummih<br />
-		<a href="http://www.hallakol.com/">Halla Kolbeinsdóttir</a> | @hallakol<br />
-		Halldór Eldjárn | @halldorel <span class="new" title="Added: 2015-07-07">* New *</span><br />
-		<a href="http://www.dojopan.net/">Halldór Örn Guðnason</a> | @dojopan<br />
+		<a href="http://www.bmson.com/">Baldvin Mar Smárason</a> | <a href="https://twitter.com/intent/user?screen_name=@bmson" title="@bmson on Twitter">@bmson</a><br />
+		Berglind Bergsdóttir | <a href="https://twitter.com/intent/user?screen_name=@beggaboo85" title="@beggaboo85 on Twitter">@beggaboo85</a><br />
+		Bergur Hallgrímsson | <a href="https://twitter.com/intent/user?screen_name=@bergur" title="@bergur on Twitter">@bergur</a><br />
+		<a href="http://bre.klaki.net/">Bjarni Rúnar Einarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@HerraBRE" title="@HerraBRE on Twitter">@HerraBRE</a><br />
+		<a href="http://www.bingimar.is/">Bjarni Ingimar Júlíusson</a> | <a href="https://twitter.com/intent/user?screen_name=@bingimar" title="@bingimar on Twitter">@bingimar</a><br />
+		<a href="http://bjarni.net/">Bjarni Sævarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@bjarnis" title="@bjarnis on Twitter">@bjarnis</a><br />
+		<a href="http://www.csc.kth.se/~bjoola/">Björgvin Ólafsson</a> | <a href="https://twitter.com/intent/user?screen_name=@bjorgvino" title="@bjorgvino on Twitter">@bjorgvino</a><br />
+		Björgvin Ragnarsson | <a href="https://twitter.com/intent/user?screen_name=@nifgraup" title="@nifgraup on Twitter">@nifgraup</a><br />
+		<a href="http://www.undo.com/bjorn">Björn Friðgeir Björnsson</a> | <a href="https://twitter.com/intent/user?screen_name=@bjornfr" title="@bjornfr on Twitter">@bjornfr</a><br />
+		Björn Ingimundarson | <a href="https://twitter.com/intent/user?screen_name=@bjossi" title="@bjossi on Twitter">@bjossi</a><br />
+		<a href="http://bthj.is/">Björn Þór Jónsson</a> | <a href="https://twitter.com/intent/user?screen_name=@bthj" title="@bthj on Twitter">@bthj</a><br />
+		<a href="http://suda.co.uk">Brian Suda</a> | <a href="https://twitter.com/intent/user?screen_name=@briansuda" title="@briansuda on Twitter">@briansuda</a><br />
+		<a href="http://egill.re/">Egill R. Erlendsson</a> | <a href="https://twitter.com/intent/user?screen_name=@e18n" title="@e18n on Twitter">@e18n</a><br />		
+		<a href="http://neu-r.com">Einar Birgir Einarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@einarbirgir" title="@einarbirgir on Twitter">@einarbirgir</a><br />
+		Einar Kristján Bridde | <a href="https://twitter.com/intent/user?screen_name=@einarkristjan" title="@einarkristjan on Twitter">@einarkristjan</a><br />		
+		<a href="http://beautyvsfunction.com/">Einar Þór Gústafsson</a> | <a href="https://twitter.com/intent/user?screen_name=@einargustafsson" title="@einargustafsson on Twitter">@einargustafsson</a><br />
+		<a href="http://einarolafsson.com/">Einar Ólafsson</a> | <a href="https://twitter.com/intent/user?screen_name=@einsiol" title="@einsiol on Twitter">@einsiol</a><br />
+		Erling Ormar Vignisson | <a href="https://twitter.com/intent/user?screen_name=@erlingormar" title="@erlingormar on Twitter">@erlingormar</a><br />
+		<a href="http://fannar.me/">Fannar Freyr Jónsson</a> | <a href="https://twitter.com/intent/user?screen_name=@fannar" title="@fannar on Twitter">@fannar</a><br />
+		<a href="http://www.gommit.com">Finnur Pálmi Magnússon</a> | <a href="https://twitter.com/intent/user?screen_name=@gommit" title="@gommit on Twitter">@gommit</a><br />
+		<a href="http://g.is/">Garðar Hauksson</a> | <a href="https://twitter.com/intent/user?screen_name=@gardarh" title="@gardarh on Twitter">@gardarh</a><br />		
+		<a href="http://geirfreysson.com/">Geir Freysson</a> | <a href="https://twitter.com/intent/user?screen_name=@geirfreysson" title="@geirfreysson on Twitter">@geirfreysson</a><br />
+		<a href="http://gudmundurbjarni.com">Guðmundur Bjarni Ólafsson</a> | <a href="https://twitter.com/intent/user?screen_name=@gudmundur" title="@gudmundur on Twitter">@gudmundur</a><br />
+		<a href="http://hreidarsson.com/">Guðmundur Hreiðarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@gudmundurh" title="@gudmundurh on Twitter">@gudmundurh</a><br />
+		Guðjón Már Guðjónsson | <a href="https://twitter.com/intent/user?screen_name=@gudjon" title="@gudjon on Twitter">@gudjon</a><br />
+		<a href="http://gummih.com/">Gummi H</a> | <a href="https://twitter.com/intent/user?screen_name=@gummih" title="@gummih on Twitter">@gummih</a><br />
+		<a href="http://www.hallakol.com/">Halla Kolbeinsdóttir</a> | <a href="https://twitter.com/intent/user?screen_name=@hallakol" title="@hallakol on Twitter">@hallakol</a><br />
+		Halldór Eldjárn | <a href="https://twitter.com/intent/user?screen_name=@halldorel" title="@halldorel on Twitter">@halldorel</a> <span class="new" title="Added: 2015-07-07">* New *</span><br />
+		<a href="http://www.dojopan.net/">Halldór Örn Guðnason</a> | <a href="https://twitter.com/intent/user?screen_name=@dojopan" title="@dojopan on Twitter">@dojopan</a><br />
 		<a href="http://hannes.agnarsson.com/">Hannes Agnarsson Johnson</a> | <a href="https://twitter.com/intent/user?screen_name=@HannesJohnson" title="@HannesJohnson on Twitter">@HannesJohnson</a><br />
-		<a href="http://www.helgi.me">Helgi Hrafn Halldórsson</a> | @harabanar<br />
-		<a href="http://hjalli.com/">Hjálmar Gíslason</a> | @hjalli<br />
-		<a href="http://www.hjaltijakobsson.com">Hjalti Jakobsson</a> | @hjaltij<br />
-		<a href="http://hlodver.com/">Hlöðver Þór Árnason</a> | @hlodver<br />
-		Hreinn Gustavsson | @hreinngu<br />
-		<a href="http://hugi.karlmenn.is/">Hugi Þórðarson</a> | @hugith<br />
-		<a href="http://ingig.net/">Ingi Gauti Ragnarsson</a> | @ingig<br />
-		Jakob Árni H. Ísleifsson | @jakobarni<br /> 
-		<a href="http://jongretar.com/">Jón Grétar Borgþórsson</a> | @JonGretar<br />
-		<a href="http://jonrh.is/">Jón Rúnar Helgason</a> | @jonrh<br />		
-		Jökull Sólberg Auðunsson | @jokull<br />
-		<a href="http://karltryggvason.com/">Karl Tryggvason</a> @karltryggvason<br />
-		<a href="http://photo.blog.is/">Kjartan Pétur Sigurðsson</a> | @photo_is</a><br /> 
-		<a href="http://www.kjartansverrisson.com/">Kjartan Sverrisson</a> | @Kjartans<br />
+		<a href="http://www.helgi.me">Helgi Hrafn Halldórsson</a> | <a href="https://twitter.com/intent/user?screen_name=@harabanar" title="@harabanar on Twitter">@harabanar</a><br />
+		<a href="http://hjalli.com/">Hjálmar Gíslason</a> | <a href="https://twitter.com/intent/user?screen_name=@hjalli" title="@hjalli on Twitter">@hjalli</a><br />
+		<a href="http://www.hjaltijakobsson.com">Hjalti Jakobsson</a> | <a href="https://twitter.com/intent/user?screen_name=@hjaltij" title="@hjaltij on Twitter">@hjaltij</a><br />
+		<a href="http://hlodver.com/">Hlöðver Þór Árnason</a> | <a href="https://twitter.com/intent/user?screen_name=@hlodver" title="@hlodver on Twitter">@hlodver</a><br />
+		Hreinn Gustavsson | <a href="https://twitter.com/intent/user?screen_name=@hreinngu" title="@hreinngu on Twitter">@hreinngu</a><br />
+		<a href="http://hugi.karlmenn.is/">Hugi Þórðarson</a> | <a href="https://twitter.com/intent/user?screen_name=@hugith" title="@hugith on Twitter">@hugith</a><br />
+		<a href="http://ingig.net/">Ingi Gauti Ragnarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@ingig" title="@ingig on Twitter">@ingig</a><br />
+		Jakob Árni H. Ísleifsson | <a href="https://twitter.com/intent/user?screen_name=@jakobarni" title="@jakobarni on Twitter">@jakobarni</a><br /> 
+		<a href="http://jongretar.com/">Jón Grétar Borgþórsson</a> | <a href="https://twitter.com/intent/user?screen_name=@JonGretar" title="@JonGretar on Twitter">@JonGretar</a><br />
+		<a href="http://jonrh.is/">Jón Rúnar Helgason</a> | <a href="https://twitter.com/intent/user?screen_name=@jonrh" title="@jonrh on Twitter">@jonrh</a><br />		
+		Jökull Sólberg Auðunsson | <a href="https://twitter.com/intent/user?screen_name=@jokull" title="@jokull on Twitter">@jokull</a><br />
+		<a href="http://karltryggvason.com/">Karl Tryggvason</a> | <a href="https://twitter.com/intent/user?screen_name=@karltryggvason" title="@karltryggvason on Twitter">@karltryggvason</a><br />
+		<a href="http://photo.blog.is/">Kjartan Pétur Sigurðsson</a> | <a href="https://twitter.com/intent/user?screen_name=@photo_is" title="@photo_is on Twitter">@photo_is</a><br /> 
+		<a href="http://www.kjartansverrisson.com/">Kjartan Sverrisson</a> | <a href="https://twitter.com/intent/user?screen_name=@Kjartans" title="@Kjartans on Twitter">@Kjartans</a><br />
 		<a href="mailto:kb[at]tct.is">Kristján B Ómarsson</a><br />
-		Kristján Ingi Mikaelsson | @kristjanmik<br />
-		<a href="http://kristjan.info/">Kristján Ævarsson</a> | @bimmi<br />
-		<a href="http://www.trymbill.is/">Maggi Trymbill</a> | @trymbill<br />
-		<a href="http://www.acoustickitty.me/">Magnús Þór Jónsson</a> | @ekkimegas<br />
+		Kristján Ingi Mikaelsson | <a href="https://twitter.com/intent/user?screen_name=@kristjanmik" title="@kristjanmik on Twitter">@kristjanmik</a><br />
+		<a href="http://kristjan.info/">Kristján Ævarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@bimmi" title="@bimmi on Twitter">@bimmi</a><br />
+		<a href="http://www.trymbill.is/">Maggi Trymbill</a> | <a href="https://twitter.com/intent/user?screen_name=@trymbill" title="@trymbill on Twitter">@trymbill</a><br />
+		<a href="http://www.acoustickitty.me/">Magnús Þór Jónsson</a> | <a href="https://twitter.com/intent/user?screen_name=@ekkimegas" title="@ekkimegas on Twitter">@ekkimegas</a><br />
 		<a href="http://www.licorice.is/">Marco Bancale</a><br />
-		Marinó A. Jónsson | @marinoj<br />
-		<a href="http://mar.anomy.net/">Már Örlygsson</a> | @maranomynet<br />
-		Ólafur Jens Ólafsson | @olijay<br />
-		<a href="http://olioskar.is/">Ólafur Óskar Egilsson</a> | @olioskar<br />		
-		<a href="http://ragnarfjolnisson.is/">Ragnar Fjölnisson</a> | @rfjolnisson<br />		
-		<a href="http://reynir.net/">Reynir Þ. Hübner</a> | @reynirhubner<br />
-		Sigtryggur A. Árnason | @sigtryggur<br />
-		<a href="http://www.svanbergsson.com/">Sigurður Árni Svanbergsson</a> | @siggiarni<br />
-		<a href="mailto:sigurjonvidar[the-at-sign]gmail[dot-here]com">Sigurjón Viðar Svavarsson</a> | @Sjonni1<br />
-		<a href="http://sindri.info/">Sindri Traustason</a> | @sindrit<br />
-		<a href="http://www.smarimccarthy.com/">Smári McCarthy</a> | @smarimc<br />
+		Marinó A. Jónsson | <a href="https://twitter.com/intent/user?screen_name=@marinoj" title="@marinoj on Twitter">@marinoj</a><br />
+		<a href="http://mar.anomy.net/">Már Örlygsson</a> | <a href="https://twitter.com/intent/user?screen_name=@maranomynet" title="@maranomynet on Twitter">@maranomynet</a><br />
+		Ólafur Jens Ólafsson | <a href="https://twitter.com/intent/user?screen_name=@olijay" title="@olijay on Twitter">@olijay</a><br />
+		<a href="http://olioskar.is/">Ólafur Óskar Egilsson</a> | <a href="https://twitter.com/intent/user?screen_name=@olioskar" title="@olioskar on Twitter">@olioskar</a><br />		
+		<a href="http://ragnarfjolnisson.is/">Ragnar Fjölnisson</a> | <a href="https://twitter.com/intent/user?screen_name=@rfjolnisson" title="@rfjolnisson on Twitter">@rfjolnisson</a><br />		
+		<a href="http://reynir.net/">Reynir Þ. Hübner</a> | <a href="https://twitter.com/intent/user?screen_name=@reynirhubner" title="@reynirhubner on Twitter">@reynirhubner</a><br />
+		Sigtryggur A. Árnason | <a href="https://twitter.com/intent/user?screen_name=@sigtryggur" title="@sigtryggur on Twitter">@sigtryggur</a><br />
+		<a href="http://www.svanbergsson.com/">Sigurður Árni Svanbergsson</a> | <a href="https://twitter.com/intent/user?screen_name=@siggiarni" title="@siggiarni on Twitter">@siggiarni</a><br />
+		<a href="mailto:sigurjonvidar[the-at-sign]gmail[dot-here]com">Sigurjón Viðar Svavarsson</a> | <a href="https://twitter.com/intent/user?screen_name=@Sjonni1" title="@Sjonni1 on Twitter">@Sjonni1</a><br />
+		<a href="http://sindri.info/">Sindri Traustason</a> | <a href="https://twitter.com/intent/user?screen_name=@sindrit" title="@sindrit on Twitter">@sindrit</a><br />
+		<a href="http://www.smarimccarthy.com/">Smári McCarthy</a> | <a href="https://twitter.com/intent/user?screen_name=@smarimc" title="@smarimc on Twitter">@smarimc</a><br />
 		<a href="http://www.facebook.com/stefan.baxter">Stefán Baxter</a><br />
-		Stefán Vignir Skarphéðinsson | @stefanvignir<br />
+		Stefán Vignir Skarphéðinsson | <a href="https://twitter.com/intent/user?screen_name=@stefanvignir" title="@stefanvignir on Twitter">@stefanvignir</a><br />
 		<a href="mailto:styrmir[at]tolvumidlun.is">Styrmir Bjarnason</a><br />		
-		Sverrir Ásmundsson | @tsverrir<br />
-		<a href="http://www.tinna.com/">Tinna Karen Gunnarsdóttir</a> | @tinnakaren<br />
+		Sverrir Ásmundsson | <a href="https://twitter.com/intent/user?screen_name=@tsverrir" title="@tsverrir on Twitter">@tsverrir</a><br />
+		<a href="http://www.tinna.com/">Tinna Karen Gunnarsdóttir</a> | <a href="https://twitter.com/intent/user?screen_name=@tinnakaren" title="@tinnakaren on Twitter">@tinnakaren</a><br />
 		<a href="http://tomas.arni.jonasson.name/">Tómas Árni Jónasson</a><br />
-		<a href="http://www.tryggvilarusson.com/">Tryggvi Lárusson</a> | @tryggvil<br />
-		<a href="http://www.facebook.com/valursve">Valur Sverrisson</a> | @valur<br />
-		Vernharður R. Sigurðsson | @vernhardur<br />
-		Viðar Svansson | @pjesi<br />
-		Vignir Jónsson | @vignir<br />
-		<a href="http://www.hugarorar.com/">Þórður Matthiasson</a> | @doddi79<br />
-		<a href="http://thorey.runarsdott.ir/">Þórey Rúnarsdóttir</a> | @thoreyrunars<br />
-		Ægir Finnsson | @finnsson<br />
-		<a href="http://www.thorsteinsson.is/">Ægir Þorsteinsson</a> | @thorsteinsson<br />
-		Ævar Örn Kvaran | @Skrekkur<br />
+		<a href="http://www.tryggvilarusson.com/">Tryggvi Lárusson</a> | <a href="https://twitter.com/intent/user?screen_name=@tryggvil" title="@tryggvil on Twitter">@tryggvil</a><br />
+		<a href="http://www.facebook.com/valursve">Valur Sverrisson</a> | <a href="https://twitter.com/intent/user?screen_name=@valur" title="@valur on Twitter">@valur</a><br />
+		Vernharður R. Sigurðsson | <a href="https://twitter.com/intent/user?screen_name=@vernhardur" title="@vernhardur on Twitter">@vernhardur</a><br />
+		Viðar Svansson | <a href="https://twitter.com/intent/user?screen_name=@pjesi" title="@pjesi on Twitter">@pjesi</a><br />
+		Vignir Jónsson | <a href="https://twitter.com/intent/user?screen_name=@vignir" title="@vignir on Twitter">@vignir</a><br />
+		<a href="http://www.hugarorar.com/">Þórður Matthiasson</a> | <a href="https://twitter.com/intent/user?screen_name=@doddi79" title="@doddi79 on Twitter">@doddi79</a><br />
+		<a href="http://thorey.runarsdott.ir/">Þórey Rúnarsdóttir</a> | <a href="https://twitter.com/intent/user?screen_name=@thoreyrunars" title="@thoreyrunars on Twitter">@thoreyrunars</a><br />
+		Ægir Finnsson | <a href="https://twitter.com/intent/user?screen_name=@thoreyrunars" title="@thoreyrunars on Twitter">@finnsson</a><br />
+		<a href="http://www.thorsteinsson.is/">Ægir Þorsteinsson</a> | <a href="https://twitter.com/intent/user?screen_name=@thorsteinsson" title="@thorsteinsson on Twitter">@thorsteinsson</a><br />
+		Ævar Örn Kvaran | <a href="https://twitter.com/intent/user?screen_name=@Skrekkur" title="@Skrekkur on Twitter">@Skrekkur</a><br />
 
 		<p id="twitter-list">You can also check out the list on Twitter: <a href="http://twitter.com/HannesJohnson/icelandic-developers" title="You can follow them all on Twitter">@HannesJohnson/icelandic-developers</a></p>
 
